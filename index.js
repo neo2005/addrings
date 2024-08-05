5310789f933d8eec69ad469aa7080f5015f14ea3
francisco.diazr <francisco.diazr@vass.es>
2024-08-05 23:57:47 +0200

    nuevos rings

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ Hooks.on("initializeDynamicTokenRingConfig", (n) => {
   });
   n.addConfig("RuthengeRing", s);
 });
+
+Hooks.on("initializeDynamicTokenRingConfig", (n) => {
+  const s = new foundry.canvas.tokens.DynamicRingData({
+    label: "Bloodlord Ring",
+    effects: {
+      RING_PULSE: "TOKEN.RING.EFFECTS.RING_PULSE",
+      RING_GRADIENT: "TOKEN.RING.EFFECTS.RING_GRADIENT",
+      BKG_WAVE: "TOKEN.RING.EFFECTS.BKG_WAVE",
+      INVISIBILITY: "TOKEN.RING.EFFECTS.INVISIBILITY"
+    },
+    spritesheet: "modules/addrings/rings/bloodlords/sprite-sheet.json"
+  });
+  n.addConfig("BloodlordRing", s);
+});
+
+Hooks.on("initializeDynamicTokenRingConfig", (n) => {
+  const s = new foundry.canvas.tokens.DynamicRingData({
+    label: "Seven Dooms Ring",
+    effects: {
+      RING_PULSE: "TOKEN.RING.EFFECTS.RING_PULSE",
+      RING_GRADIENT: "TOKEN.RING.EFFECTS.RING_GRADIENT",
+      BKG_WAVE: "TOKEN.RING.EFFECTS.BKG_WAVE",
+      INVISIBILITY: "TOKEN.RING.EFFECTS.INVISIBILITY"
+    },
+    spritesheet: "modules/addrings/rings/sevendooms/sprite-sheet.json"
+  });
+  n.addConfig("SevenDoomsRing", s);
+});
