22c98a17d01d4a028933c1a0b19ecfde297e8d5a
francisco.diazr <francisco.diazr@vass.es>
2024-08-06 08:45:37 +0200

    sin efectos

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 Hooks.on("initializeDynamicTokenRingConfig", (n) => {
   const s = new foundry.canvas.tokens.DynamicRingData({
     label: "Rusthenge Ring",
-    effects: {
-      RING_PULSE: "TOKEN.RING.EFFECTS.RING_PULSE",
-      RING_GRADIENT: "TOKEN.RING.EFFECTS.RING_GRADIENT",
-      BKG_WAVE: "TOKEN.RING.EFFECTS.BKG_WAVE",
-      INVISIBILITY: "TOKEN.RING.EFFECTS.INVISIBILITY"
-    },
     spritesheet: "modules/addrings/rings/rusthenge/sprite-sheet.json"
   });
   n.addConfig("RuthengeRing", s);
